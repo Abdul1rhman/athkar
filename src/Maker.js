d75885b6fcd4cefbff34555fd198ebaef8f40168
Abdul1rhman <pain008900@gmail.com>
2023-04-02 14:14:41 +0300

    fix counter

--- src/Maker.js
+++ src/Maker.js
@@ -2,16 +2,62 @@ import React, { useEffect, useState } from 'react'
 import ReactAudioPlayer from 'react-audio-player';
 
 function Maker({data , handelClick,text}) {
- 
+  
+  // const [list2, setList2] = useState([]);
 
- 
+  // useEffect(() => {
+  //   // Map over list1 and create a new array with different content in each object
+  //   const newList = data.map(obj => ({
+  //     co: 0,
+      
+  //   }));
+
+  //   // Set the state of list2 to the new array
+  //   setList2(newList);
+  // }, [data]);
+
+ const [count, setCount]=useState([
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+  {"co":0},
+ ])
+const handleIncrement=(index,limit)=>{
+  const updatedItems = [...count];
+  if(updatedItems[index].co<limit){
+    updatedItems[index].co += 1;
+    setCount(updatedItems);
+  }
+    
+}
+
     return (
     <div className='adhker-cat1'>
-        {data.map(dkher =>(
-            <div className="dhker1" key={dkher.ID}>
+        {data.map((dkher, index) =>(
+            <div className={`dhker1 ${count[index].co===dkher.REPEAT? "done":''}`} key={dkher.ID} onClick={() => handleIncrement(index,dkher.REPEAT)}>
               {/* {dkher.title && <h4>( {dkher.ARABIC_TEXT} )</h4>} */}
               <p style={{fontSize:text}}>{dkher.ARABIC_TEXT}</p>
-              {/* {dkher.REPEAT===1?'':<p className='counter' onClick={()=>handelCount(dkher.ID)}>{dkher.REPEAT}</p>} */}
+              {dkher.REPEAT===1?'':<p className='counter' >{count[index].co}</p>}
               <ReactAudioPlayer
                 src={dkher.AUDIO}
                 controls/>
